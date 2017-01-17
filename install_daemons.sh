03cb28ce579537c958cf72b8f0e56994ddd1d43f
Jake Osterholt <jacobo@uoregon.edu>
2017-01-17 05:29:40 +0000

    Test of installing postgres and Apache httpd.

--- install_daemons.sh
+++ install_daemons.sh
@@ -7,3 +7,10 @@ git checkout -b REL9_5_STABLE origin/REL9_5_STABLE
 make
 make install
 cd ..
+curl http://mirrors.gigenet.com/apache//httpd/httpd-2.4.25.tar.bz2 > httpd-2.4.25.tar.bz2
+tar -xjf httpd-2.4.25.tar.bz2
+cd httpd-2.4.25
+./configure --prefix=$1 --with-port=8080
+make
+make install
+cd ..
