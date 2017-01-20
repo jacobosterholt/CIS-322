b7c1d6883efe896764158a9c073ab938da7df840
Jake Osterholt <jacobo@uoregon.edu>
2017-01-20 05:24:50 +0000

    Fix a typo.

--- sql/import_data.sh
+++ sql/import_data.sh
@@ -2,7 +2,7 @@
 
 # Download the legacy data
 export https_proxy=http://proxy:$2/
-curl -o osnap_legacy.tar.gz https:classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz
+curl -o osnap_legacy.tar.gz https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz
 
 # Unzip the lagacy data
 tar -zxvf osnap_legacy.tar.gz
